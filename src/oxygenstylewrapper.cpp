af393e724603a0ce2309137d76471c920169283a
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-10-19 14:05:48 -0600

    coding style.

--- src/oxygenstylewrapper.cpp
+++ src/oxygenstylewrapper.cpp
@@ -649,10 +649,18 @@ namespace Oxygen
 
             StyleOptions options( widget, state, shadow );
             if(GTK_IS_PROGRESS_BAR(widget))
+            {
+
                 Style::instance().renderProgressBarHandle( window, clipRect, x, y, w, h, options );
-            else // most likely it's progressbar in the list
+
+            } else {
+
+                // most likely it's progressbar in the list
                 // FIXME: is it always the case?
                 Style::instance().renderProgressBarHandle( window, clipRect, x, y+2, w, h-4, options );
+
+            }
+
             return;
 
         } else {
@@ -769,12 +777,13 @@ namespace Oxygen
             options |= NoFill;
             Style::instance().renderSlab(window,clipRect,x-2,y-2,w+4,h+2, options );
 
-        } else if ( (GTK_IS_TREE_VIEW(widget) || GTK_IS_CELL_VIEW(widget)) && shadow==GTK_SHADOW_IN)
-        {
+        } else if ( (GTK_IS_TREE_VIEW(widget) || GTK_IS_CELL_VIEW(widget)) && shadow==GTK_SHADOW_IN) {
+
             // it's likely progressbar hole
             // FIXME: is it enough to check for TreeView? is shadow_in the only possible case?
             StyleOptions options;
             Style::instance().renderProgressBarHole(window,clipRect,x-2,y,w+4,h,options);
+
         } else if( shadow == GTK_SHADOW_IN && !Gtk::gtk_parent_statusbar( widget ) ) {
 
             // default shadow_in frame
