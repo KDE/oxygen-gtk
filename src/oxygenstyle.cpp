668e49dd40f0df1ced978236761fd2ee63930dea
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-10-31 19:16:41 -0400

    fixed arrow spacing when calculating scrollbar holes.

--- src/oxygenstyle.cpp
+++ src/oxygenstyle.cpp
@@ -2807,7 +2807,7 @@ namespace Oxygen
     void Style::adjustScrollBarHole( int& x, int& y, int& w, int& h, StyleOptions options ) const
     {
 
-        const int buttonSize( 14 );
+        const int buttonSize( 15 );
         const int subLineOffset( buttonSize*settings().scrollBarSubLineButtons() );
         const int addLineOffset( buttonSize*settings().scrollBarAddLineButtons() );
         if( options&Vertical )
