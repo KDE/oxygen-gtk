f13a735d4fbad102979de69b0de90393fce51efb
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-11-04 10:44:15 -0400

    check whether widget is enabled when setting initial hover value, at connection.

--- src/animations/oxygenhoverdata.cpp
+++ src/animations/oxygenhoverdata.cpp
@@ -38,12 +38,23 @@ namespace Oxygen
         std::cout << "Oxygen::HoverData::connect - " << widget << " (" << G_OBJECT_TYPE_NAME( widget ) << ")" << std::endl;
         #endif
 
+        const bool enabled( gtk_widget_get_state( widget ) != GTK_STATE_INSENSITIVE );
+
         // on connection, needs to check whether mouse pointer is in widget or not
         // to have the proper initial value of the hover flag
-        gint xPointer,yPointer;
-        gdk_window_get_pointer(widget->window,&xPointer,&yPointer, 0L);
-        GdkRectangle rect = { 0, 0, widget->allocation.width, widget->allocation.height };
-        setHovered( widget, Gtk::gdk_rectangle_contains( &rect, xPointer, yPointer ) );
+        if( enabled )
+        {
+
+            gint xPointer,yPointer;
+            gdk_window_get_pointer(widget->window,&xPointer,&yPointer, 0L);
+            GdkRectangle rect = { 0, 0, widget->allocation.width, widget->allocation.height };
+            setHovered( widget, Gtk::gdk_rectangle_contains( &rect, xPointer, yPointer ) );
+
+        } else {
+
+            setHovered( widget, false );
+
+        }
 
         // register callbacks
         _enterId = g_signal_connect( G_OBJECT(widget), "enter-notify-event", G_CALLBACK( enterNotifyEvent ), this );
