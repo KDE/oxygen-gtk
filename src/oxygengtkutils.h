2e9558bd6e76cad2383870ef6cbad20375c11f08
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-11-17 18:01:48 +0100

    Revert "Revert gtk button order in dialogs"
    This reverts commit 5393f026ff64bba91039186e6b6198616af7b205.
    
    Conflicts:
    
    	src/oxygengtkutils.h
    	src/oxygenstylewrapper.cpp

--- src/oxygengtkutils.h
+++ src/oxygengtkutils.h
@@ -224,9 +224,6 @@ namespace Oxygen
                 ( rect->y <= y && (rect->y + rect->height) > y );
         }
 
-        //! gtk error handler, do nothing
-        inline void oxygen_log_handler( const gchar*, GLogLevelFlags, const gchar*, gpointer )
-        {}
         //@}
 
     }
