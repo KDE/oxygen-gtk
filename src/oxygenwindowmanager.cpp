f3868b638892874122fb47e25584b6e5d74730d9
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-12-20 17:34:18 +0100

    Fixed storage of rejected events when testing whether a drag should be started or not. CCBUG: 260008

--- src/oxygenwindowmanager.cpp
+++ src/oxygenwindowmanager.cpp
@@ -153,16 +153,18 @@ namespace Oxygen
         if( event->type == GDK_BUTTON_PRESS && event->button == Gtk::LeftButton )
         {
 
+            const bool accepted( static_cast<WindowManager*>(data)->isWindowDragWidget( widget, event ) );
             #if OXYGEN_DEBUG
             std::cout << "Oxygen::WindowManager::wmButtonPress -"
                 << " event: " << event
                 << " widget: " << widget
                 << " (" << G_OBJECT_TYPE_NAME( widget ) << ")"
                 << " " << gtk_widget_get_name( widget )
+                << " accepted: " << accepted
                 << std::endl;
             #endif
 
-            return static_cast<WindowManager*>(data)->isWindowDragWidget( widget, event );
+            return accepted;
 
         } else return false;
 
@@ -219,7 +221,13 @@ namespace Oxygen
             _drag = true;
             return true;
 
-        } else return false;
+        } else {
+
+            // mark event as rejected
+            _lastRejectedEvent = event;
+            return false;
+        }
+
     }
 
     //_________________________________________________________________
@@ -367,7 +375,6 @@ namespace Oxygen
 
         }
 
-        if( !useEvent ) _lastRejectedEvent = event;
         return useEvent;
 
     }
@@ -422,8 +429,9 @@ namespace Oxygen
             { usable = childrenUseEvent( childWidget, event, inNoteBook); }
 
             #if OXYGEN_DEBUG
-            std::cout << "Oxygen::WindowManager::childrenUseEvent - "
-                << widget
+            std::cout << "Oxygen::WindowManager::childrenUseEvent -"
+                << " event: " << event
+                << " widget: " << widget
                 << " (" << G_OBJECT_TYPE_NAME( childWidget ) << ")"
                 << " " << gtk_widget_get_name( childWidget )
                 << " usable: " << usable
