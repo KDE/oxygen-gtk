7e033a7e3fa9fd9d80bbd9a12b57da1463fc2bce
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-12-24 13:43:59 +0100

    use MainToolBar icon size for drag and drop. The drag and drop icon size is not defined in KDE specs, so the choice is arbitrary. Since the choice above fixes some bug in handbrake, lets pick this one.

--- src/oxygenqtsettings.cpp
+++ src/oxygenqtsettings.cpp
@@ -87,7 +87,7 @@ namespace Oxygen
 
         if( g_getenv( "KDE_FULL_SESSION" ) )
             _KDESession = true;
-        
+
         // clear RC
         _rc.clear();
 
@@ -132,7 +132,7 @@ namespace Oxygen
             // reload fonts
             loadKdeFonts();
         }
-        
+
         // kde globals options
         loadKdeGlobalsOptions();
 
@@ -338,7 +338,7 @@ namespace Oxygen
         icons.setIconSize( "panel", panelIconSize );
         icons.setIconSize( "gtk-small-toolbar", toolbarIconSize );
         icons.setIconSize( "gtk-large-toolbar", mainToolbarIconSize );
-        icons.setIconSize( "gtk-dnd", desktopIconSize );
+        icons.setIconSize( "gtk-dnd", mainToolbarIconSize );
         icons.setIconSize( "gtk-button", smallIconSize );
         icons.setIconSize( "gtk-menu", smallIconSize );
         icons.setIconSize( "gtk-dialog", dialogIconSize );
