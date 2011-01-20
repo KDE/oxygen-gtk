dd576db1f0ed0692a353d2c80eb77cf2e6a330aa
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-01-20 14:43:51 +0100

    disabled registration of GtkOptionMenu to engines.

--- src/oxygenstylewrapper.cpp
+++ src/oxygenstylewrapper.cpp
@@ -546,8 +546,8 @@ namespace Oxygen
 
         } else if( d.isButton() || d.isOptionMenu() || d.isToggleButton() ) {
 
-            if( d.isOptionMenu() && GTK_IS_OPTION_MENU( widget ) )
-            { Style::instance().animations().optionMenuEngine().registerWidget( widget ); }
+            // if( d.isOptionMenu() && GTK_IS_OPTION_MENU( widget ) )
+            // { Style::instance().animations().optionMenuEngine().registerWidget( widget ); }
 
             // check if it's PathBar toggle button
             if( Gtk::gtk_button_is_in_path_bar(widget) )
