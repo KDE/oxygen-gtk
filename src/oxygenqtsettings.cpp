28ccee2f3017b51d3c5a83411839013a94d8391d
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-01-24 23:13:46 +0100

    Fixed rendering of pathbar buttons arrow in RTL mode.
    gtkrc: removed oxygen-pathbar style. It is moved to hardcoded oxygenqtsettings, to be
    able to deal with RTL.
    
    oxygenstylewrapper: fixed positionning and orientation of arrow. Fixed selected rect for
    last button.

--- src/oxygenqtsettings.cpp
+++ src/oxygenqtsettings.cpp
@@ -130,6 +130,24 @@ namespace Oxygen
         loadKdeIcons();
         #endif
 
+        // deal with pathbar button margins
+        // this needs to be done programatically in order to properly account for RTL locales
+        _rc.addSection( "oxygen-pathbutton", Gtk::RC::defaultSection() );
+        _rc.addToCurrentSection( "  GtkButton::inner-border = { 2, 2, 1, 0 }" );
+
+        if( gtk_widget_get_default_direction() == GTK_TEXT_DIR_RTL )
+        {
+
+            _rc.addToCurrentSection( "  GtkToggleButton::inner-border={ 10, 0, 1, 0 }" );
+
+        } else {
+
+            _rc.addToCurrentSection( "  GtkToggleButton::inner-border={ 0, 10, 1, 0 }" );
+
+        }
+
+        _rc.addToRootSection( "widget_class \"*PathBar.GtkToggleButton\" style \"oxygen-pathbutton\"" );
+
         #if OXYGEN_DEBUG
         std::cerr << "Oxygen::QtSettings::initialize - Gtkrc: " << std::endl;
         std::cerr << _rc << std::endl;
