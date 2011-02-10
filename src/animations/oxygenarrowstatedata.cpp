219ed13019ee367c13d53c7bbbe0c6aa576cfcf2
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-02-10 15:06:48 +0100

    added Oxygen::ArrowStateData to handle GtkSpinButton and GtkNotebook arrow animation.

---
+++ src/animations/oxygenarrowstatedata.cpp
@@ -0,0 +1,95 @@
+/*
+* this file is part of the oxygen gtk engine
+* Copyright (c) 2010 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or(at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include "oxygenarrowstatedata.h"
+#include "../config.h"
+
+#include <iostream>
+
+namespace Oxygen
+{
+
+    //_____________________________________________
+    void ArrowStateData::connect( GtkWidget* widget )
+    {
+
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::ArrowStateData::connect - " << widget << " (" << G_OBJECT_TYPE_NAME( widget ) << ")" << std::endl;
+        #endif
+
+        _target = widget;
+        _upArrowData._timeLine.connect( (GSourceFunc)delayedUpdate, this );
+        _downArrowData._timeLine.connect( (GSourceFunc)delayedUpdate, this );
+    }
+
+    //_____________________________________________
+    void ArrowStateData::disconnect( GtkWidget* widget )
+    {
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::ArrowStateData::disconnect - " << widget << " (" << G_OBJECT_TYPE_NAME( widget ) << ")" << std::endl;
+        #endif
+
+        _upArrowData._timeLine.disconnect();
+        _upArrowData._state = false;
+
+        _downArrowData._timeLine.disconnect();
+        _downArrowData._state = false;
+
+        _target = 0L;
+
+    }
+
+    //_____________________________________________
+    bool ArrowStateData::Data::updateState( bool state )
+    {
+        if( state == _state ) return false;
+
+        #if OXYGEN_DEBUG
+        std::cerr
+            << "Oxygen::ArrowStateData::Data::updateState - "
+            << " state: " << state
+            << std::endl;
+        #endif
+
+        _state = state;
+
+        // change direction
+        _timeLine.setDirection( state ? TimeLine::Forward:TimeLine::Backward );
+
+        // restart timeLine if needed
+        if( _timeLine.isConnected() && !_timeLine.isRunning() ) _timeLine.start();
+
+        return true;
+    }
+
+    //_____________________________________________
+    gboolean ArrowStateData::delayedUpdate( gpointer pointer )
+    {
+
+        ArrowStateData& data( *static_cast<ArrowStateData*>( pointer ) );
+
+        if( data._target )
+        { gtk_widget_queue_draw( data._target ); }
+
+        return FALSE;
+
+    }
+
+}
---
+++ src/animations/oxygenarrowstatedata.h
@@ -0,0 +1,145 @@
+#ifndef oxygenarrowstatedata_h
+#define oxygenarrowstatedata_h
+/*
+* this file is part of the oxygen gtk engine
+* Copyright (c) 2010 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or(at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include "oxygentimeline.h"
+
+#include <gtk/gtk.h>
+
+namespace Oxygen
+{
+    //! track arrow state changes events,
+    /*! used to deal with spinbox and Notebook arrows */
+    class ArrowStateData
+    {
+
+        public:
+
+        //! constructor
+        explicit ArrowStateData( void ):
+            _target( 0L )
+        {}
+
+        //! destructor
+        virtual ~ArrowStateData( void )
+        {}
+
+        //! setup connections
+        void connect( GtkWidget* );
+
+        //! disconnect
+        void disconnect( GtkWidget* );
+
+        //! update state
+        bool updateState( GtkArrowType type, bool value )
+        { return data( type ).updateState( value ); }
+
+        //! return timeline
+        const TimeLine& timeLine( GtkArrowType type ) const
+        { return data( type )._timeLine; }
+
+        //! return timeline
+        TimeLine& timeLine( GtkArrowType type )
+        { return data( type )._timeLine; }
+
+        //! opacity
+        double opacity( GtkArrowType type ) const
+        { return data( type )._timeLine.value(); }
+
+        protected:
+
+        // arrow data
+        class Data
+        {
+
+            public:
+
+            //! constructor
+            explicit Data( void ):
+                _state( false )
+            {}
+
+            //! update state
+            bool updateState( bool );
+
+            //! timeline
+            TimeLine _timeLine;
+
+            //! state
+            bool _state;
+
+        };
+
+        //! get data for given arrow type
+        const Data& data( GtkArrowType type ) const
+        {
+            switch( type )
+            {
+
+                case GTK_ARROW_UP:
+                case GTK_ARROW_LEFT:
+                return _upArrowData;
+
+                default:
+                case GTK_ARROW_DOWN:
+                case GTK_ARROW_RIGHT:
+                return _downArrowData;
+
+            }
+        }
+
+        //! get data for given arrow type
+        Data& data( GtkArrowType type )
+        {
+            switch( type )
+            {
+
+                case GTK_ARROW_UP:
+                case GTK_ARROW_LEFT:
+                return _upArrowData;
+
+                default:
+                case GTK_ARROW_DOWN:
+                case GTK_ARROW_RIGHT:
+                return _downArrowData;
+
+            }
+        }
+
+        //! delayed update
+        static gboolean delayedUpdate( gpointer );
+
+        private:
+
+        //! target
+        GtkWidget* _target;
+
+        // up/left arrow
+        Data _upArrowData;
+
+        // down/right arrow
+        Data _downArrowData;
+
+    };
+
+}
+
+#endif
