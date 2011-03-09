95781967396b4974afa4e03f26165dc0cb7b8a4b
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-03-09 08:31:35 +0100

    Added class for simple generation of CSS options.

---
+++ src/oxygengtkcss.cpp
@@ -0,0 +1,155 @@
+/*
+* this file is part of the oxygen gtk engine
+* Copyright (c) 2010 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or( at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include "oxygengtkcss.h"
+
+#include <gtk/gtk.h>
+#include <algorithm>
+#include <cassert>
+
+namespace Oxygen
+{
+
+    //_________________________________________________
+    void Gtk::CSS::commit( void )
+    {
+        GtkCssProvider* provider( gtk_css_provider_get_default() );
+
+        GError *error( 0L );
+        const std::string contents( toString() );
+        gtk_css_provider_load_from_data( provider, contents.c_str(), contents.size(), &error );
+        if( error )
+        {
+            std::cerr << "Oxygen::CSS::commit - error reported while parsing: " << std::endl;
+            std::cerr << error->message << std::endl;
+            g_error_free( error );
+        }
+
+        clear();
+    }
+
+    //_________________________________________________
+    void Gtk::CSS::merge( const Gtk::CSS& other )
+    {
+
+        // loop over sections in other
+        for( Section::List::const_iterator iter = other._sections.begin(); iter != other._sections.end(); ++iter )
+        {
+            Section::List::iterator sectionIter = std::find_if( _sections.begin(), _sections.end(), Section::SameNameFTor( *iter ) );
+            if( sectionIter == _sections.end() ) _sections.push_back( *iter );
+            else {
+
+                sectionIter->add( iter->_content );
+
+            }
+
+        }
+
+        return;
+    }
+
+    //_________________________________________________
+    void Gtk::CSS::addSection( const std::string& name )
+    {
+        if( std::find( _sections.begin(), _sections.end(), name ) != _sections.end() )
+        {
+
+            std::cerr << "Gtk::CSS::addSection - section named " << name << " already exists" << std::endl;
+
+        } else {
+
+            _sections.push_back( Section( name ) );
+
+        }
+
+        setCurrentSection( name );
+
+    }
+
+    //_________________________________________________
+    void Gtk::CSS::addToSection( const std::string& name, const std::string& content )
+    {
+        Section::List::iterator iter( std::find( _sections.begin(), _sections.end(), name ) );
+        if( iter == _sections.end() )
+        {
+            std::cerr << "Gtk::CSS::addToSection - unable to find section named " << name << std::endl;
+            return;
+        }
+
+        iter->add( content );
+    }
+
+    //_________________________________________________
+    void Gtk::CSS::setCurrentSection( const std::string& name )
+    {
+        if( std::find( _sections.begin(), _sections.end(), name ) == _sections.end() )
+        {
+
+            std::cerr << "Gtk::CSS::setCurrentSection - unable to find section named " << name << std::endl;
+            return;
+
+        } else {
+
+            _currentSection = name;
+
+        }
+    }
+
+    //_________________________________________________
+    void Gtk::CSS::Section::add( const Gtk::CSS::Section::ContentList& content )
+    {
+        for( ContentList::const_iterator iter = content.begin(); iter != content.end(); ++iter )
+        {
+            if( std::find( _content.begin(), _content.end(), *iter ) == _content.end() )
+            { _content.push_back( *iter ); }
+        }
+    }
+
+    namespace Gtk
+    {
+        //_______________________________________________________________________
+        std::ostream& operator << (std::ostream& out, const CSS::Section& section )
+        {
+
+            out << section._name << " {";
+            out << std::endl;
+
+            // add contents
+            for( CSS::Section::ContentList::const_iterator iter = section._content.begin(); iter != section._content.end(); ++iter )
+            { out << *iter << std::endl; }
+
+            out << "}" << std::endl;
+            return out;
+
+        }
+
+        //_______________________________________________________________________
+        std::ostream& operator << (std::ostream& out, const CSS& rc )
+        {
+
+            // dump all sections
+            for( CSS::Section::List::const_iterator iter = rc._sections.begin(); iter != rc._sections.end(); ++iter )
+            { out << *iter << std::endl; }
+
+            return out;
+        }
+
+    }
+}
---
+++ src/oxygengtkcss.h
@@ -0,0 +1,204 @@
+#ifndef oxygengtkrc_h
+#define oxygengtkrc_h
+/*
+* this file is part of the oxygen gtk engine
+* Copyright (c) 2010 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or( at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace Oxygen
+{
+    namespace Gtk
+    {
+
+        //! convenience class to generate GtkCSS option
+        template< typename T>
+            class CSSOption
+        {
+            public:
+
+            //! constructor
+            CSSOption( std::string name, const T& value )
+            {
+                std::ostringstream stream;
+                stream << name << ": " << value << ";";
+                _value = stream.str();
+            }
+
+            //! cast to cairo_t
+            operator const std::string& (void) const
+            { return _value; }
+
+            private:
+
+            std::string _value;
+
+        };
+
+        //! handle Gtk CSS style sheet generation
+        class CSS
+        {
+
+            public:
+
+            //! constructor
+            explicit CSS( void )
+            { init(); }
+
+            //! destructor
+            virtual ~CSS( void )
+            {}
+
+            //! clear
+            void clear( void )
+            {
+                _sections.clear();
+                init();
+            }
+
+
+            //! merge
+            void merge( const CSS& );
+
+            //! create new section and set as current
+            void addSection( const std::string& name );
+
+            //! set current section
+            void setCurrentSection( const std::string& name );
+
+            //! add line to section
+            void addToSection( const std::string& name, const std::string& content );
+
+            //! add line to current section
+            void addToCurrentSection( const std::string& content )
+            { addToSection( _currentSection, content ); }
+
+            //! convert to string
+            std::string toString( void ) const
+            {
+                std::ostringstream out;
+                out << *this << std::endl;
+                return out.str();
+            }
+
+            //! commit to gtk and clear
+            void commit( void );
+
+            protected:
+
+            //! initialize default sections
+            void init( void )
+            {}
+
+            //! describes each style section in resource list
+            class Section
+            {
+
+                public:
+
+                //! list
+                typedef std::list<Section> List;
+
+                //! empty constructor
+                explicit Section( void )
+                {}
+
+                //! constructor
+                explicit Section( const std::string& name ):
+                    _name( name )
+                {}
+
+                //! clear
+                void clear( void )
+                { _content.clear(); }
+
+                //! add to content
+                void add( const std::string& content )
+                {
+                    if( !content.empty() )
+                    _content.push_back( content );
+                }
+
+                //! contents list shortcut
+                typedef std::vector<std::string> ContentList;
+
+                //! add to content
+                void add( const ContentList& );
+
+                //! equal operator. Based on name only
+                bool operator == (const Section& other ) const
+                { return other._name == _name; }
+
+                //! equal operator. Based on name only
+                bool operator == (const std::string& other ) const
+                { return other == _name; }
+
+                //! name
+                std::string _name;
+
+                //! content
+                ContentList _content;
+
+                //! used to find section with matching name
+                class SameNameFTor
+                {
+                    public:
+
+                    //! constructor
+                    SameNameFTor( const Section& section ):
+                        _name( section._name )
+                        {}
+
+                    //! predicate
+                    bool operator() ( const Section& other ) const
+                    { return _name == other._name; }
+
+                    private:
+
+                    //! prediction
+                    std::string _name;
+
+                };
+
+
+            };
+
+            private:
+
+            //! list of sections
+            Section::List _sections;
+
+            //! current section
+            std::string _currentSection;
+
+            //! streamer
+            friend std::ostream& operator << ( std::ostream&, const Section& );
+
+            //! streamer
+            friend std::ostream& operator << ( std::ostream&, const CSS& );
+
+        };
+    }
+
+}
+#endif
