809c329841cdd8ff9ac679f181645174821fab84
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-03-14 10:05:26 +0100

    fixed vertical positionning of spinbox arrows.

--- src/oxygenthemingengine.cpp
+++ src/oxygenthemingengine.cpp
@@ -1731,9 +1731,6 @@ namespace Oxygen
             if( Gtk::gtk_theming_engine_layout_is_reversed( engine ) ) x+=1;
             else x-=1;
 
-            if( arrow == GTK_ARROW_UP ) y -= 1;
-            else if( arrow == GTK_ARROW_DOWN ) y -= 2;
-
             // disable contrast
             options &= ~Contrast;
 
