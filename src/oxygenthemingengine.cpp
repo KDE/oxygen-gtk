35957695d0f011fe9b84cc938b4ef5446e310373
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-03-15 16:14:56 +0100

    Revert "added offsets when rendering combobox entry. To be removed when bug is fixed upstream."
    This reverts commit 8d42433d030e731e4c868ca23e2590d43664209e.

--- src/oxygenthemingengine.cpp
+++ src/oxygenthemingengine.cpp
@@ -1096,11 +1096,6 @@ namespace Oxygen
                 // TODO: use correct Palette::Group depending on state
                 const ColorUtils::Rgba background( Style::instance().settings().palette().color( Palette::Base ) );
 
-                // TODO: remove when bug is fixed upstream
-                int offset( ( h + 2 - Gtk::gtk_widget_get_allocation( parent ).height )/2 );
-                y += offset;
-                h -= 2*offset;
-
                 if( Gtk::gtk_theming_engine_layout_is_reversed( engine ) )
                 {
 
