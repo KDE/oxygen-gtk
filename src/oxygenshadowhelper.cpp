6d8a15654d1ddb5bee7f1ca408c66d64cec150dc
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-04-05 14:24:34 +0200

    implemented shadows for tooltips.

--- src/oxygenshadowhelper.cpp
+++ src/oxygenshadowhelper.cpp
@@ -164,7 +164,8 @@ namespace Oxygen
             hint == GDK_WINDOW_TYPE_HINT_MENU ||
             hint == GDK_WINDOW_TYPE_HINT_DROPDOWN_MENU ||
             hint == GDK_WINDOW_TYPE_HINT_POPUP_MENU ||
-            hint == GDK_WINDOW_TYPE_HINT_COMBO;
+            hint == GDK_WINDOW_TYPE_HINT_COMBO ||
+            hint == GDK_WINDOW_TYPE_HINT_TOOLTIP;
     }
 
     //______________________________________________
