a4a2e2dcec34ac857ab0e06ad7283978a21b6f3f
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-04-06 18:42:41 +0200

    fixed rendering of slider through.

--- src/oxygenthemingengine.cpp
+++ src/oxygenthemingengine.cpp
@@ -548,25 +548,30 @@ namespace Oxygen
 
             if( gtk_theming_engine_has_class( engine, GTK_STYLE_CLASS_CELL ) || gtk_widget_path_is_type( path, GTK_TYPE_PROGRESS_BAR ) )
             {
+
                 StyleOptions options(widget, state);
                 if(GTK_IS_PROGRESS_BAR(widget) && Gtk::gtk_widget_is_vertical(widget))
                     options|=Vertical;
                 Style::instance().renderProgressBarHole( context, x, y, w, h, options );
                 return;
-            }
 
-            if( gtk_theming_engine_has_class( engine, GTK_STYLE_CLASS_SCALE ) && GTK_IS_SCALE( widget ) )
-            {
+            } else if( gtk_theming_engine_has_class( engine, GTK_STYLE_CLASS_SCALE ) && GTK_IS_SCALE( widget ) ) {
+
                 const bool vertical( Gtk::gtk_widget_is_vertical( widget ) );
                 const int offset( 6 );
                 if( vertical ) {
 
                     Style::instance().renderSliderGroove( context, x, y + offset, w, h - 2*offset, Vertical );
+
                 } else {
 
                     Style::instance().renderSliderGroove( context, x + offset, y, w - 2*offset, h, StyleOptions() );
                 }
+
+                return;
+
             }
+
 
         }
 
