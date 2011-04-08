aedeb02e0a244389d4c45e9debaca6978ec3d42f
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-04-08 10:48:20 +0200

    added the possibility of delaying background updates for scrolledwindow children on scrollbar value changed events.

--- src/animations/oxygenscrollbardata.cpp
+++ src/animations/oxygenscrollbardata.cpp
@@ -28,18 +28,82 @@ namespace Oxygen
 
     //________________________________________________________________________________
     void ScrollBarData::connect( GtkWidget* widget )
-    { _valueChangedId.connect( G_OBJECT(widget), "value-changed", G_CALLBACK( valueChanged ), widget ); }
+    {
+        _target = widget;
+        _updatesDelayed = true;
+        _valueChangedId.connect( G_OBJECT(widget), "value-changed", G_CALLBACK( valueChanged ), this );
+    }
 
     //________________________________________________________________________________
     void ScrollBarData::disconnect( GtkWidget* widget )
-    { _valueChangedId.disconnect(); }
+    {
+        _target = 0L;
+
+        // reset timeout and locked flag
+        _timer.stop();
+        _locked = false;
+
+        _valueChangedId.disconnect();
+    }
 
     //________________________________________________________________________________
-    void ScrollBarData::valueChanged( GtkRange* widget, gpointer data )
+    void ScrollBarData::valueChanged( GtkRange* widget, gpointer pointer )
     {
-        if( GtkWidget* parent = Gtk::gtk_parent_scrolled_window( GTK_WIDGET( widget ) ) )
-        { gtk_widget_queue_draw( static_cast<GtkWidget*>(parent) ); }
+
+        ScrollBarData& data( *static_cast<ScrollBarData*>( pointer ) );
+        if( data._updatesDelayed )
+        {
+
+            // schedule delayed timeOut
+            if( !data._timer.isRunning() )
+            {
+
+                data._timer.start( data._delay, (GSourceFunc)delayedUpdate, &data );
+                data._locked = false;
+
+            } else data._locked = true;
+
+
+        } else if( GtkWidget* parent = Gtk::gtk_parent_scrolled_window( GTK_WIDGET( widget ) ) ) {
+
+            gtk_widget_queue_draw( parent );
+
+        }
+
         return;
+    }
+
+    //________________________________________________________________________________
+    gboolean ScrollBarData::delayedUpdate( gpointer pointer )
+    {
+
+        ScrollBarData& data( *static_cast<ScrollBarData*>(pointer) );
+
+        if( !data._target )
+        {
+
+            // if target is invalid, reset timeout and return
+            data._locked = false;
+            return FALSE;
+
+        } else if( data._locked ) {
+
+            // if locked, reset the flag and re-run timer
+            data._locked = false;
+            return TRUE;
+
+        } else if( GtkWidget* parent = Gtk::gtk_parent_scrolled_window( GTK_WIDGET( data._target ) ) ) {
+
+            // otherwise, trigger update
+            gtk_widget_queue_draw( parent );
+            return FALSE;
+
+        }
+
+        // if everything fails, unlock and do nothing
+        data._locked = false;
+        return FALSE;
+
     }
 
 }
