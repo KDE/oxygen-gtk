fa6a6b648d2a862948fccb082ea81ce8cdedd0bc
Ruslan Kabatsayev <b7.10110111@gmail.com>
2011-04-17 21:45:00 +0400

    Don't do debug output unless requested

--- src/oxygengtkutils.cpp
+++ src/oxygengtkutils.cpp
@@ -78,7 +78,9 @@ namespace Oxygen
     {
         if( !widget ) return false;
 
-        std::cerr << "is_applet(): " << Gtk::gtk_widget_path(widget) << std::endl;
+        #if OXYGEN_DEBUG
+        std::cerr << "Gtk::gtk_widget_is_applet(): " << Gtk::gtk_widget_path(widget) << std::endl;
+        #endif
 
         static const char* names[] =
         {
