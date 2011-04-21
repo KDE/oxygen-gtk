22a4ec9517c7afeec428922c207b318757bdfd0c
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-04-22 00:03:04 +0200

    fixed hole rendering.

--- src/oxygenstylehelper.cpp
+++ src/oxygenstylehelper.cpp
@@ -677,7 +677,7 @@ namespace Oxygen
             TileSet(
                 shadowSurface, shadowSize, shadowSize, shadowSize, shadowSize,
                 shadowSize-1, shadowSize, 2, 1 ).
-                render( context, 0, 0, size*2, size*2, TileSet::Full );
+                render( context, 0, 0, size*2, size*2 );
 
         }
 
