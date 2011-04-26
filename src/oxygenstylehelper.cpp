cf77e11a429a438a65f575840870753f319e5850
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-04-26 11:23:21 +0200

    fixed progressbar handle rendering.

--- src/oxygenstylehelper.cpp
+++ src/oxygenstylehelper.cpp
@@ -1022,7 +1022,6 @@ namespace Oxygen
         {
             // filling
             cairo_set_source( context, ColorUtils::mix( highlight, dark, 0.2 ) );
-            cairo_rectangle( context, xl, yl, wl, hl );
             cairo_rounded_rectangle( context, xl, yl, wl, hl, 2.5 );
             cairo_fill( context );
         }
