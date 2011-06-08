df32e8392dfdcca687f0ebc93d20f4f0793a99f7
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-06-08 15:42:37 +0200

    removed version check, for gtk3 (since naturally fullfilled)

--- src/animations/oxygenpanedengine.cpp
+++ src/animations/oxygenpanedengine.cpp
@@ -89,13 +89,8 @@ namespace Oxygen
         {
 
             // load handle window
-            #if GTK_CHECK_VERSION(2, 20, 0)
-            GdkWindow* window(  gtk_paned_get_handle_window( GTK_PANED( widget ) ) );
-            #else
-            GdkWindow* window( GTK_PANED( widget )->handle );
-            #endif
-
             // assign cursor
+            GdkWindow* window(  gtk_paned_get_handle_window( GTK_PANED( widget ) ) );
             gdk_window_set_cursor( window, cursor );
 
             return true;
