1b376729de067f40b3d5988105ee0fcf93f500b9
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-07-06 16:09:02 +0200

    fixed GTK_THEME_DIR variable name.

--- config.h.cmake
+++ config.h.cmake
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define GTK_THEME_DIR "@GTK_THEME_DIR@/gtk-2.0"
+#define GTK_THEME_DIR "@GTK_THEME_DIR@/gtk-3.0"
 #ifndef OXYGEN_DEBUG
 #   define OXYGEN_DEBUG @OXYGEN_DEBUG@
 #endif
