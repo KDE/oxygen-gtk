160f0884890bcbd5f85ad777016acaf79c4636db
Ruslan Kabatsayev <b7.10110111@gmail.com>
2012-02-19 17:43:41 +0400

    Use .view instead of Gtk*View since it applies to all such widgets, including nauilus' folder view CCBUG: 294390

--- src/oxygenqtsettings.cpp
+++ src/oxygenqtsettings.cpp
@@ -623,7 +623,7 @@ namespace Oxygen
         _css.addToCurrentSection( Gtk::CSSOption<std::string>( GTK_STYLE_PROPERTY_COLOR, _palette.color( Palette::Disabled, Palette::WindowText ) ) );
 
         // entries
-        _css.addSection( "GtkEntry, GtkTreeView, GtkTextView, GtkIconView" );
+        _css.addSection( ".view, GtkEntry" );
         _css.addToCurrentSection( Gtk::CSSOption<std::string>( GTK_STYLE_PROPERTY_BACKGROUND_COLOR, _palette.color( Palette::Base ) ) );
         _css.addToCurrentSection( Gtk::CSSOption<std::string>( GTK_STYLE_PROPERTY_COLOR, _palette.color( Palette::Text ) ) );
 
