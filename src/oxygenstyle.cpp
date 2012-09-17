690ebcbc61e0f469c3aaddec8f04d16d0b1aa98b
Ruslan Kabatsayev <b7.10110111@gmail.com>
2012-09-17 23:28:51 +0400

    Fix maximized windeco border in composited state

--- src/oxygenstyle.cpp
+++ src/oxygenstyle.cpp
@@ -2436,7 +2436,7 @@ namespace Oxygen
         bool drawAlphaChannel( wopt & WinDeco::DrawAlphaChannel );
         StyleOptions options( hasAlpha ? Alpha : Blend );
 
-        if( hasAlpha )
+        if( hasAlpha && !isMaximized )
         {
             // cut round corners using alpha
             cairo_rounded_rectangle(context,x,y,w,h,3.5);
