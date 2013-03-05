678d800280cf27db0e38e9d3985f167fb7a41414
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2013-03-05 16:19:49 +0100

    Removed comment about invalid tilesets, since it will never be reached.

--- src/oxygentileset.cpp
+++ src/oxygentileset.cpp
@@ -95,9 +95,6 @@ namespace Oxygen
         initSurface( _surfaces, surface, w, _h3, x1, y2, w2, _h3 );
         initSurface( _surfaces, surface, _w3, _h3, x2, y2, _w3, _h3 );
 
-        if(!isValid())
-            std::cerr << "oxygen-gtk: BUG: created tileset looks invalid, expect resource leaks ("<<__FILE__<<":"<<__LINE__<<")\n";
-
     }
 
     //______________________________________________________________
