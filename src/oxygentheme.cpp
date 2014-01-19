ab845ffde7d8097a7bd2f2c5b9a12b4504f49964
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2014-01-19 20:30:55 +0100

    commented out the add_quit function

--- src/oxygentheme.cpp
+++ src/oxygentheme.cpp
@@ -62,8 +62,8 @@ void theme_init( GTypeModule* module )
     // style initialization
     Oxygen::Style::instance().initialize();
 
-    // add quit function to make sure theme is de-allocated properly
-    gtk_quit_add( gtk_main_level(), theme_exit_callback, 0x0 );
+//     // add quit function to make sure theme is de-allocated properly
+//     gtk_quit_add( gtk_main_level(), theme_exit_callback, 0x0 );
 
 }
 
